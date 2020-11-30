371da3b591d20a504405f3857a4866909539f5d4
rockycsumb <romoreno@csumb.edu>
2020-11-30 23:37:21 +0000

    add pages

---
+++ client/src/components/pages/Products/Products.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import HeroSection from '../../HeroSection';
+import Pricing from '../../Pricing';
+import {homeObjFour} from './Data';
+
+function Products() {
+	return(
+		<>
+			<HeroSection {...homeObjFour} />
+		</>
+	)
+}
+
+export default Products;
