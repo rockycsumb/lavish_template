a2594feb6bc2cf182f5d6e40c63bb31c7cbc3d83
rockycsumb <romoreno@csumb.edu>
2020-12-01 06:48:44 +0000

    added css transition

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,35 @@
 import './App.css';
 import Navbar from './components/Navbar';
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import {BrowserRouter as Router, Route, Switch, withRouter} from 'react-router-dom';
 import Home from './components/pages/HomePage/Home';
 import Services from './components/pages/Services/Services';
 import Products from './components/pages/Products/Products';
 import SignUp from './components/pages/SignUp/SignUp';
 import Footer from './components/pages/Footer/Footer';
 import ScrollToTop from './ScrollToTop';
+import { TransitionGroup, CSSTransition } from "react-transition-group";
+import './transitionGroup.css';
+
+
+const AnimatedSwitch = withRouter(({location})=>(
+	<TransitionGroup>
+		<CSSTransition key={location.key} classNames="slide" timeout={1000}>
+			<Switch location={location}>
+			  <Route path="/" exact component={Home}/>
+			  <Route path="/services" component={Services}/>
+			  <Route path="/products" component={Products}/>
+			  <Route path="/signup" component={SignUp}/>
+		  	</Switch>
+		</CSSTransition>
+	</TransitionGroup>
+));
 
 function App() {
   return (
     <Router>
       <Navbar />
 		  <ScrollToTop />
-		  <Switch>
-			  <Route path="/" exact component={Home}/>
-			  <Route path="/services" component={Services}/>
-			  <Route path="/products" component={Products}/>
-			  <Route path="/signup" component={SignUp}/>
-		  </Switch>
+		  <AnimatedSwitch />
 		  <Footer />
     </Router>
   );
